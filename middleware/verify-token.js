0a0181be12427caa747073e34def52ef7a0e4c47
AbdelRhman Ibrahim <114269455+AbdelrhmanJaber@users.noreply.github.com>
2025-02-21 00:24:40 +0200

    JWT

---
+++ middleware/verify-token.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const httpStatus = require("../utils/https_status_text");
+const appError = require("../utils/appError");
+
+const verifyToken = (req, res, next) => {
+  const authHeader =
+    req.headers["Authorization"] || req.headers["authorization"];
+  if (!authHeader) {
+    const error = appError.create("Token is required", 401, httpStatus.ERROR);
+    return next(error);
+  }
+  const token = authHeader.split(" ")[1];
+  console.log("token", token);
+  try {
+    const currentUser = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.currentUser = currentUser;
+    next();
+  } catch (err) {
+    const error = appError.create("invalid token", 401, httpStatus.ERROR);
+    return next(error);
+  }
+};
+
+module.exports = verifyToken;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const validationSchemaFunc = require("../middleware/validationSchema");
 const router = express.Router();
 const userController = require("../controllers/users-controllers");
+const verifyToken = require("../middleware/verify-token");
 
-router.route("/").get(userController.getAllUsers);
+router.route("/").get(verifyToken, userController.getAllUsers);
 
 router.route("/register").get(userController.register);
 
